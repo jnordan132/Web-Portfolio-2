f33320d3e9d9aafdb5d7ef5a8cb276c0eaee7cc8
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-07-30 19:59:15 -0400

    added all sections content in html, added css as well for all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Header Functionality
 $(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
+    if ($(window).scrollTop() > 200) {
         $('.main_nav').addClass('sticky');
     } else {
         $('.main_nav').removeClass('sticky');
